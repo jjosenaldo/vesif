473483926d214325679e1e289b31c58b43cac7e0
Josenaldo <josenaldo.asj@gmail.com>
2024-05-14 23:13:51 -0300

    migrate to Compose with Gradle

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,42 @@
-package org.example
-
-import org.example.core.model.examples.example01
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
 import org.example.core.model.examples.exampleWithShortCircuit
 import org.example.csp_generator.generator.CspGenerator
 import org.example.verifier.fdr.Verifier
 import org.example.verifier.model.AssertionType
 import java.nio.file.Paths
 
-fun main() {
+@Composable
+@Preview
+fun App() {
+    var text by remember { mutableStateOf("Hello, World!") }
+
+    MaterialTheme {
+        Button(onClick = {
+            text = "Hello, Desktop!"
+        }) {
+            Text(text)
+        }
+    }
+}
+
+fun main() = application {
+    main2()
+    Window(onCloseRequest = ::exitApplication) {
+        App()
+    }
+}
+
+fun main2() {
     val circuit = exampleWithShortCircuit
 
     CspGenerator.generateCsp(
