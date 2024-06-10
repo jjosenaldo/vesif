8cb8facc7377ec9cab575cb3bab8cc5188b2384f
Josenaldo <josenaldo.asj@gmail.com>
2024-06-09 23:36:22 -0300

    center initial button

--- src/main/kotlin/ui/screens/select_project/SelectProjectScreen.kt
+++ src/main/kotlin/ui/screens/select_project/SelectProjectScreen.kt
@@ -1,11 +1,15 @@
 package ui.screens.select_project
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.darkrockstudios.libraries.mpfilepicker.DirectoryPicker
 import kotlinx.coroutines.launch
 import org.koin.compose.koinInject
@@ -26,7 +30,11 @@ fun SelectProjectScreen(
         }
     }
 
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         when (state) {
             is SelectProjectError -> TODO()
             is SelectProjectInitial -> Button(onClick = {
